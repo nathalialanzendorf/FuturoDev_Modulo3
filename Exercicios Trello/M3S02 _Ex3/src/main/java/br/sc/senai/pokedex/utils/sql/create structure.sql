a86a7f1c13430739db6704916754b45f661479ec
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-06-10 21:04:01 -0300

    [M3S02] Ex. 3 - Criar funcionalidade de cadastro de pokémon

--- Exercicios Trello/M3S02 _Ex3/src/main/java/br/sc/senai/pokedex/utils/sql/create structure.sql
+++ Exercicios Trello/M3S02 _Ex3/src/main/java/br/sc/senai/pokedex/utils/sql/create structure.sql
@@ -1,71 +1,46 @@
-DROP DATABASE imob;
-CREATE DATABASE imob;
+DROP DATABASE pokedex;
+CREATE DATABASE pokedex;
 
-DROP TABLE IF EXISTS imovel;
-DROP TABLE IF EXISTS users_roles;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS pokemon;
 
-
-CREATE TABLE imovel (
-    id UUID PRIMARY KEY,
-    descricao VARCHAR(255),
-    tipo VARCHAR(255),
-    endereco VARCHAR(255),
-    bairro VARCHAR(255),
-    cidade VARCHAR(255),
-    valor DECIMAL(10,2),
-    data_cadastro Timestamp
-);
-
-CREATE TABLE roles (
-    id UUID PRIMARY KEY,
-    name VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    enabled BOOLEAN DEFAULT TRUE,
-    locked BOOLEAN DEFAULT FALSE,
-    account_date_expire Timestamp,
-    credential_date_expire Timestamp,
-    FOREIGN KEY (id) REFERENCES roles(id)
+CREATE TABLE pokemon (
+    id uuid PRIMARY KEY,
+    numero integer not null,
+    nome varchar(255) not null, 
+    descricao varchar(255),
+    imagem varchar(255), 
+    tipo varchar(255),
+    categoria varchar(255), 
+    peso float(53), 
+    area varchar(255), 
+    altura float(53), 
+    capturado boolean not null
 );
 
 
-CREATE TABLE users_roles (
-    user_id UUID NOT NULL,
-    role_id UUID NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id)
-);
-
-INSERT INTO roles (id, name) VALUES (
+INSERT INTO pokemon (id, numero, nome, descricao, imagem, area, capturado) VALUES (
 	'0b581629-3c80-4af9-9bcd-b6f79a095736',
-	'ADMIN');
-INSERT INTO users (id, username, password) 
-VALUES (
-		'0b581629-3c80-4af9-9bcd-b6f79a095736',
-		'admin',
-		'$2a$10$rJV1yLaC5lYEiZCMMfdCpOafv1apFJPAyJ.svpTJFMQdoabusQhDa');
-INSERT INTO users_roles (user_id, role_id) VALUES (
-	'0b581629-3c80-4af9-9bcd-b6f79a095736',
-	'0b581629-3c80-4af9-9bcd-b6f79a095736');
-	
-INSERT INTO roles (id, name) VALUES (
-	'bc655bd7-fb35-44aa-8787-6ab3edc01bd7',
-	'READ');
-INSERT INTO users (id, username, password) 
-VALUES (
-		'bc655bd7-fb35-44aa-8787-6ab3edc01bd7',
-		'nathalia',
-		'$2a$10$rJV1yLaC5lYEiZCMMfdCpOafv1apFJPAyJ.svpTJFMQdoabusQhDa');
-INSERT INTO users_roles (user_id, role_id) VALUES (
-	'bc655bd7-fb35-44aa-8787-6ab3edc01bd7',
-	'bc655bd7-fb35-44aa-8787-6ab3edc01bd7');
-	
-	SELECT * FROM users;
-	SELECT * FROM roles;
-	SELECT * FROM users_roles;
+    '132',
+	'Ditto',
+    'Pikachu',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png',
+    'Area de floresta',
+    'false'
+);
+
+
+INSERT INTO pokemon (id, numero, nome, descricao,imagem, tipo, categoria, area, altura, peso, capturado) VALUES (
+	'c30a90d2-83d8-4905-8db7-c5862eb3e43d',
+    '25',
+	'Pikachu',
+    'Pikachu',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+    'ELETRICO',
+    'RATO',
+    'Area de floresta',
+    '4',
+    '6.0',
+    true
+);
+
+SELECT * FROM pokemon;
