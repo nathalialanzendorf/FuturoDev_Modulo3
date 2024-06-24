bbf9d63674dd90da6834a0d22960c1c438ff1ffa
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2024-06-24 20:12:16 -0300

    Modulo 3 Semana 3 Aula 1

---
+++ Exercicios Aula/M3S03 - Aula 1/docker-compose/src/test/java/com/senai/docker/Aula2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
